5a79497ebb426255ad0d8b509203058d0c1b0dad
zlocate <zlocated@gmail.com>
2022-05-31 12:27:15 +0300

    feat: added tags following schema field & api endpoints

--- models/User.js
+++ models/User.js
@@ -1,46 +1,92 @@
-var mongoose = require('mongoose');
-var uniqueValidator = require('mongoose-unique-validator');
-var crypto = require('crypto');
-var jwt = require('jsonwebtoken');
-var secret = require('../config').secret;
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config');
 
-var UserSchema = new mongoose.Schema({
-  username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-  nickname: String,
-  email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-  bio: String,
-  image: String,
-  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
-  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  hash: String,
-  salt: String
-}, {timestamps: true, usePushEach: true});
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+      index: true,
+    },
+    nickname: String,
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, 'is invalid'],
+      index: true,
+    },
+    bio: {
+      type: String,
+      default: '',
+      maxlength: [250, 'max length 250 symbols'],
+    },
+    image: String,
+    favorites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article',
+      default: []
+      },
+    ],
+    followingUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    followingTags: [
+      {
+        type: String
+      },
+    ],
+    hash: String,
+    salt: String,
+  },
+  { timestamps: true, usePushEach: true },
+);
 
-UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 
-UserSchema.methods.validPassword = function(password) {
-  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+UserSchema.methods.validPassword = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+    .toString('hex');
+
   return this.hash === hash;
 };
 
-UserSchema.methods.setPassword = function(password){
+UserSchema.methods.setPassword = function (password) {
   this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+    .toString('hex');
 };
 
-UserSchema.methods.generateJWT = function() {
-  var today = new Date();
-  var exp = new Date(today);
+UserSchema.methods.generateJWT = function () {
+  const today = new Date(),
+    exp = new Date(today);
+
   exp.setDate(today.getDate() + 60);
 
-  return jwt.sign({
-    id: this._id,
-    username: this.username,
-    exp: parseInt(exp.getTime() / 1000),
-  }, secret);
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      exp: parseInt(exp.getTime() / 1000, 10),
+    },
+    secret,
+  );
 };
 
-UserSchema.methods.toAuthJSON = function(){
+UserSchema.methods.toAuthJSON = function () {
   return {
     username: this.username,
     email: this.email,
@@ -51,52 +97,72 @@ UserSchema.methods.toAuthJSON = function(){
   };
 };
 
-UserSchema.methods.toProfileJSONFor = function(user){
+UserSchema.methods.toProfileJSONFor = function (user) {
   return {
     username: this.username,
     nickname: this.nickname,
     bio: this.bio,
-    image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
-    following: user ? user.isFollowing(this._id) : false
+    image:
+      this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
+    following: user ? user.isFollowing(this._id) : false,
+    followingTags: user.followingTags,
   };
 };
 
-UserSchema.methods.favorite = function(id){
-  if(this.favorites.indexOf(id) === -1){
+UserSchema.methods.favorite = function (id) {
+  if (this.favorites.indexOf(id) === -1) {
     this.favorites.push(id);
   }
 
   return this.save();
 };
 
-UserSchema.methods.unfavorite = function(id){
+UserSchema.methods.unfavorite = function (id) {
   this.favorites.remove(id);
   return this.save();
 };
 
-UserSchema.methods.isFavorite = function(id){
-  return this.favorites.some(function(favoriteId){
-    return favoriteId.toString() === id.toString();
-  });
+UserSchema.methods.isFavorite = function (id) {
+  return this.favorites.some(
+    (favoriteId) => favoriteId.toString() === id.toString(),
+  );
 };
 
-UserSchema.methods.follow = function(id){
-  if(this.following.indexOf(id) === -1){
-    this.following.push(id);
+UserSchema.methods.follow = function (id) {
+  if (this.followingUsers.indexOf(id) === -1) {
+    this.followingUsers.push(id);
   }
 
   return this.save();
 };
 
-UserSchema.methods.unfollow = function(id){
-  this.following.remove(id);
+UserSchema.methods.unfollow = function (id) {
+  this.followingUsers.remove(id);
   return this.save();
 };
 
-UserSchema.methods.isFollowing = function(id){
-  return this.following.some(function(followId){
-    return followId.toString() === id.toString();
-  });
+UserSchema.methods.followTag = function (id) {
+  if (!this.followingTags.includes(id)) {
+    this.followingTags.push(id);
+  }
+
+  return this.save();
+};
+
+UserSchema.methods.unfollowTag = function (id) {
+  this.followingTags.remove(id);
+  return this.save();
+};
+
+
+UserSchema.methods.isFollowing = function (id) {
+  return this.followingUsers.some(
+    (followId) => followId.toString() === id.toString(),
+  );
+};
+
+UserSchema.methods.isFollowingTag = function (id) {
+  return this.followingTags.includes(id);
 };
 
 mongoose.model('User', UserSchema);
--- routes/api/tags.js
+++ routes/api/tags.js
@@ -1,12 +1,65 @@
-var router = require('express').Router();
-var mongoose = require('mongoose');
-var Article = mongoose.model('Article');
+const router = require('express').Router();
+const mongoose = require('mongoose');
+const auth = require('../auth');
+const User = mongoose.model('User');
+const Article = mongoose.model('Article');
 
-// return a list of tags
-router.get('/', function(req, res, next) {
-  Article.find().distinct('tagList').then(function(tags){
-    return res.json({tags: tags});
-  }).catch(next);
+// Return a list of tags
+router.get('/', (req, res, next) => {
+  Article.find()
+    .distinct('tagList')
+    .then((tags) => res.json({ tags }))
+    .catch(next);
 });
 
+
+router.get('/follow',  auth.required, (req, res, next) => {
+  User.findById(req.payload.id)
+  .then((user) => {
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    return res.json({tags: user.followingTags})
+  });
+});
+
+router.post('/:tag/follow', auth.required, function (req, res, next) {
+  const tag = req.params.tag;
+  console.log(tag);
+  User.findById(req.payload.id)
+    .then((user) => {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      return user
+        .followTag(tag)
+        .then(() => res.status(204).send());
+    })
+    .catch(next);
+});
+
+router.delete('/:tag/follow', auth.required, function (req, res, next) {
+  const tag = req.params.tag;
+
+  User.findById(req.payload.id)
+    .then((user) => {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+      cons
+
+      return user
+        .unfollowTag(tag)
+        .then(() => res.status(204).send());
+    })
+    .catch(next);
+});
+
+router.get('/top', async (req, res, next) => {
+  try {
+    const tags = await Article.aggregate([{ $project: { tagList: 1 } }, { $unwind: '$tagList' }, { $group: { _id: '$tagList', count: { $sum: 1 } } }, { $sort: { count: -1 } }, { $limit: 20 }, { $project: { _id: 0, name: '$_id', count: 1 } }]).exec();
+    return res.json({ tags });
+  } catch (err) { return next(err); }
+});
 module.exports = router;
