1399cb750123d68551791a80f7310e5a89ce7f80
zlocate <zlocated@gmail.com>
2022-05-31 12:24:18 +0300

    feat: update express package, rix indent

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-var http = require('http'),
-    path = require('path'),
-    methods = require('methods'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cors = require('cors'),
-    passport = require('passport'),
-    errorhandler = require('errorhandler'),
-    mongoose = require('mongoose');
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const mongoose = require('mongoose');
 
-var isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 // Create global app object
-var app = express();
+const app = express();
 
 app.use(cors());
 
 // Normal express config defaults
 app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(require('method-override')());
-app.use(express.static(__dirname + '/public'));
-app.use(session({ secret: 'kitchen', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+
+app.use(
+  session({
+    secret: 'kitchen',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
 if (!isProduction) {
   app.use(errorhandler());
 }
 
-if(isProduction){
+if (isProduction) {
   mongoose.connect(process.env.MONGODB_URI);
 } else {
   mongoose.connect('mongodb://kogda-virastu-mongodb:27017/kitchen');
@@ -43,41 +46,48 @@ require('./config/passport');
 
 app.use(require('./routes'));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+// / catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+
   err.status = 404;
   next(err);
 });
 
-/// error handlers
+// / error handlers
 
-// development error handler
-// will print stacktrace
+/*
+ * Development error handler
+ * will print stacktrace
+ */
 if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
-
+  app.use((err, req, res) => {
     res.status(err.status || 500);
 
-    res.json({'errors': {
-      message: err.message,
-      error: err
-    }});
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+/*
+ * Production error handler
+ * no stacktraces leaked to user
+ */
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.json({'errors': {
-    message: err.message,
-    error: {}
-  }});
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
 });
 
-// finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, function(){
+// Finally, let's start our server...
+const server = app.listen(process.env.PORT || 3000, function(){
   console.log('Listening on port ' + server.address().port);
 });
