8391877e4c63f47774a6250c010e2f6c05673438
zlocate <zlocated@gmail.com>
2022-06-03 16:18:01 +0300

    fix: return 409 (conflict) error on unique error

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,10 +7,11 @@ router.use('/tags', require('./tags'));
 
 router.use(function(err, req, res, next){
   if(err.name === 'ValidationError'){
-    return res.status(422).json({
+    const key = Object.keys(err.errors)[0];
+    const statusCode = err?.errors[key]?.kind === 'unique' ? 409 : 400;
+    return res.status(statusCode).json({
       errors: Object.keys(err.errors).reduce(function(errors, key){
         errors[key] = err.errors[key].message;
-
         return errors;
       }, {})
     });
