adee51c4a4f64eac0ac0f18a508bc5bd477d0197
zlocate <zlocated@gmail.com>
2022-06-07 03:12:44 +0300

    feat: ignore tags for posts pending moderation

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -1,31 +1,31 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
 const auth = require('../auth');
+
 const User = mongoose.model('User');
 const Article = mongoose.model('Article');
 
 // Return a list of tags
 router.get('/', (req, res, next) => {
-  Article.find()
+  Article.find({ state: 'published' })
     .distinct('tagList')
     .then((tags) => res.json({ tags }))
     .catch(next);
 });
 
-
-router.get('/follow',  auth.required, (req, res, next) => {
+router.get('/follow', auth.required, (req, res, next) => {
   User.findById(req.payload.id)
-  .then((user) => {
-    if (!user) {
-      return res.sendStatus(401);
-    }
-    return res.json({tags: user.followingTags})
-  });
+    .then((user) => {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+      return res.json({ tags: user.followingTags });
+    });
 });
 
-router.post('/:tag/follow', auth.required, function (req, res, next) {
-  const tag = req.params.tag;
-  console.log(tag);
+router.post('/:tag/follow', auth.required, (req, res, next) => {
+  const { tag } = req.params;
+
   User.findById(req.payload.id)
     .then((user) => {
       if (!user) {
@@ -39,8 +39,8 @@ router.post('/:tag/follow', auth.required, function (req, res, next) {
     .catch(next);
 });
 
-router.delete('/:tag/follow', auth.required, function (req, res, next) {
-  const tag = req.params.tag;
+router.delete('/:tag/follow', auth.required, (req, res, next) => {
+  const { tag } = req.params;
 
   User.findById(req.payload.id)
     .then((user) => {
@@ -57,13 +57,12 @@ router.delete('/:tag/follow', auth.required, function (req, res, next) {
 router.get('/top', async (req, res, next) => {
   try {
     const tags = await Article.aggregate([
-      { $project: { tagList: 1 } }, 
-      { $unwind: '$tagList' }, 
-      { $group: { _id: '$tagList', count: { $sum: 1 } } }, 
-      { $sort: { count: -1 } }, 
-      { $limit: 20 }, 
-      { $project: { _id: 0, name: '$_id', count: 1 } 
-    }]).exec();
+      { $project: { tagList: 1 } },
+      { $unwind: '$tagList' },
+      { $group: { _id: '$tagList', count: { $sum: 1 } } },
+      { $sort: { count: -1 } },
+      { $limit: 20 },
+      { $project: { _id: 0, name: '$_id', count: 1 } }]).exec();
     return res.json({ tags });
   } catch (err) { return next(err); }
 });
